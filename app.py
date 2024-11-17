9ff1adff82678196035923b61bbb36727a42ab4a
karislicem <karislicem99@gmail.com>
2024-11-17 18:01:20 +0300

    Update app.py
    auto installer chrome

--- app.py
+++ app.py
@@ -1,10 +1,8 @@
 from flask import Flask, jsonify
 from selenium import webdriver
-from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.common.by import By
-from webdriver_manager.chrome import ChromeDriverManager
 from selenium.common.exceptions import NoSuchElementException, TimeoutException
+import chromedriver_autoinstaller  # ChromeDriver'ı otomatik kurar
 import time
 from datetime import datetime
 
@@ -18,12 +16,16 @@ CACHE_DURATION = 1800  # Önbelleğin süresi (30 dakika = 1800 saniye)
 
 # Selenium ile döviz kurlarını çekme fonksiyonu
 def get_exchange_rates():
+    # ChromeDriver'ı otomatik kur
+    chromedriver_autoinstaller.install()
+
+    # Selenium için Chrome ayarları
     options = Options()
     options.add_argument('--headless')  # Headless mod
     options.add_argument('--no-sandbox')  # Bazı sunucu kısıtlamaları için
     options.add_argument('--disable-dev-shm-usage')  # Düşük bellek kullanımı için
-    service = Service(ChromeDriverManager().install())  # ChromeDriver yolunu otomatik ayarlama
-    driver = webdriver.Chrome(service=service, options=options)
+
+    driver = webdriver.Chrome(options=options)
     try:
         driver.get("https://www.ziraatbank.com.tr/tr")
         dolar_alis = float(driver.find_element(By.XPATH, "//h3[text()='BANKA ALIŞ']/following-sibling::span").text.strip().replace(',', '.'))
